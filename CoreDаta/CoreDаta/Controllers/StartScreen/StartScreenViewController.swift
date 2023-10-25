8c750db367010ea84a24206fc3778ab4a76aeb72
LitvinovichRoman <127106050+yeerryy@users.noreply.github.com>
2023-10-25 22:12:23 +0300

    prt1
    - repeated class work
    - fix request.predicate in CategoryTVC

---
+++ CoreDаta/CoreDаta/Controllers/StartScreen/StartScreenViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StartScreenViewController.swift
+//  CoreDаta
+//
+//  Created by Roman Litvinovich on 25/10/2023.
+//
+
+import UIKit
+import Lottie
+
+final class StartScreenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
